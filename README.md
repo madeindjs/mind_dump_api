fbde36ebf60899be123abd865791811eafbd1fc4
Alexandre Rousseau <contact@rousseau-alexandre.Fr>
2019-10-14 13:40:50 +0200

    Improve imports

--- README.md
+++ README.md
@@ -4,8 +4,28 @@ Export all your thoughts and let Natural Language Processing [Spacy](https://spa
 
 ## Usage
 
+### Parse thoughts
+
 ~~~bash
 $ python3 mind_dump.py "Bonjour Lorène, je pense que je vais passer te voir cette après midi à Croix Rousse"
+$ # OR
+$ echo "Bonjour Lorène, je pense que je vais passer te voir cette après midi à Croix Rousse" | python3 mind_dump.py
+~~~
+
+### Export
+
+It will highlight recognized word:
+
+- `+` for persons
+- `@` for locations
+- `:` for organisations
+- `?` for others
+
+Exemple:
+
+~~~bash
+$ python3 export.py
+2019-10-14 13:26 - Bonjour +[Lorène], je pense que je vais passer te voir cette après midi à @[Croix Rousse]
 ~~~
 
 ## Installation
--- mind_dump.py
+++ mind_dump.py
@@ -1,25 +1,39 @@
 import sys
 from mind_dump.models import Though, Word, ThoughWord
 from mind_dump.parse import parse_sentence
+from sqlite3 import IntegrityError
 
 
 if __name__ == '__main__':
-    sentence = sys.argv[1:][0]
-    assert sentence, 'You must provide something'
 
-    though = Though.create(content=sentence)
+    if sys.argv[1:]:
+        sentences = sys.argv[1:][0]
+    elif sys.stdin:
+        sentences = ""
+        for line in sys.stdin:
+            sentences += line
 
-    for entity in parse_sentence(sentence):
+    assert sentences, 'You must provide something'
 
-        word = None
 
-        try:
-            word = Word.get(Word.content == entity.text)
-        except:
-            word = Word.create(label=entity.label_, content=entity.text)
 
-        though_word = ThoughWord.create(word=word, though=though)
-        though_word.save()
+    for sentence in sentences.splitlines():
+        though = Though.create(content=sentence)
+
+        for entity in parse_sentence(sentence):
+            word = None
+
+            try:
+                word = Word.get(Word.content == entity.text)
+            except:
+                word = Word.create(label=entity.label_, content=entity.text)
+
+            try:
+                though_word = ThoughWord.create(word=word, though=though)
+                though_word.save()
+            except:
+                pass
+
 
     though.parsed = True
     though.save()
