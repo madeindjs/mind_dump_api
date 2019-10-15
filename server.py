ed8be84925145bcbd847adb8d5ab6239917c0911
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-10-15 22:11:32 +0200

    Add CORS

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, escape, request, jsonify
+from flask_cors import CORS
 from add import import_sentence
 from mind_dump.models import Though, ThoughWord, Word
 import mistune
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
